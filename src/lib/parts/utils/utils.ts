49f68e5f880ece5e56f12c3214fd664d7907fa83
SegFault <LCozzoli@users.noreply.github.com>
2022-12-21 15:31:43 +0100

    Thanks to nishuzumi

--- src/lib/parts/utils/utils.ts
+++ src/lib/parts/utils/utils.ts
@@ -3,11 +3,16 @@ import { use } from 'typescript-mix';
 import { waitForCode } from './code';
 import { Countries } from './countries';
 import { Services } from './services';
+import { Query } from '../../query/query.module';
 
 export interface Utils extends waitForCode {}
 
 @singleton()
 export class Utils {
   @use(waitForCode) this;
-  constructor(public countries: Countries, public services: Services) {}
+  constructor(
+    public countries: Countries,
+    public services: Services,
+    public readonly query: Query
+  ) {}
 }
