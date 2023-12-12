8dda775d6dd8948a23628fe78bc4fc7d7179abd2
yellows <efremovsergey09@gmail.com>
2023-12-12 17:20:08 +0300

    changes

--- src/lib/parts/activations/routes/getNumberV2.ts
+++ src/lib/parts/activations/routes/getNumberV2.ts
@@ -34,7 +34,9 @@ export class getNumberV2 {
       this.query
         ?.makeCall(EApiActions.getNumber, options)
         .then((response) => {
-            console.log("Get number response " + response)
+          if (process.env.SMS_ACTIVATE_DEBUG){
+              console.log("Get number response " + response)
+          }
           if (typeof response == 'object') {
             return resolve(
               new SMSNumber({
